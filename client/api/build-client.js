53e6a479849ffb5d975f43aa88987469a9e33a93
Supasin <palsp2406@gmail.com>
2021-02-13 15:51:44 +0700

    init

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,12 +1,20 @@
 import axios from "axios";
 
+<<<<<<< Updated upstream
 const buildClient = ({ req }) => {
+=======
+export default ({ req }) => {
+>>>>>>> Stashed changes
   if (typeof window === "undefined") {
     // We are on the server
 
     return axios.create({
       baseURL:
+<<<<<<< Updated upstream
         "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+=======
+        "http://ingress-nginx-controllerr.ingress-nginx.svc.cluster.local",
+>>>>>>> Stashed changes
       headers: req.headers,
     });
   } else {

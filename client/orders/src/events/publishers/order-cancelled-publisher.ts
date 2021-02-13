7de46e68f2a70623ce08e79f9d9e7e886ba3157d
Supasin <palsp2406@gmail.com>
2021-02-13 15:43:49 +0700

    update client

--- client/orders/src/events/publishers/order-cancelled-publisher.ts
+++
@@ -1,5 +0,0 @@
-import { Subjects, Publisher, OrderCancelledEvent } from '@sgtickets/common';
-
-export class OrderCancelledPublisher extends Publisher<OrderCancelledEvent> {
-  subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
-}
--- client/orders/src/events/publishers/order-created-publisher.ts
+++
@@ -1,5 +0,0 @@
-import { Publisher, OrderCreatedEvent, Subjects } from '@sgtickets/common';
-
-export class OrderCreatedPublisher extends Publisher<OrderCreatedEvent> {
-  subject: Subjects.OrderCreated = Subjects.OrderCreated;
-}
